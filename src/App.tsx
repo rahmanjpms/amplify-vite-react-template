f63f8c748bdd36ea92166c002d4dfcecfc376a10
jpms_rahman <rahman@jpms.co.jp>
2024-11-29 09:44:29 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 const client = generateClient<Schema>();
 
 function App() {
-    const { signOut } = useAuthenticator();
+    // const { signOut } = useAuthenticator();
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+    const { user, signOut } = useAuthenticator();
 
     useEffect(() => {
         client.models.Todo.observeQuery().subscribe({
@@ -25,6 +27,7 @@ function App() {
 
     return (
         <main>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
             <button onClick={signOut}>Sign out</button>
             <h1>My todos</h1>
             <button onClick={createTodo}>+ new</button>
@@ -35,11 +38,7 @@ function App() {
                     </li>
                 ))}
             </ul>
-            <div>
-                🥳 App successfully hosted. Try creating a new todo.
-                <br />
-                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">Review next step of this tutorial.</a>
-            </div>
+            <div>App successfully hosted. Try creating a new todo.</div>
         </main>
     );
 }
